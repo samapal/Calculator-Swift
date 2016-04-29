324dd737289b196d77b7fd69744ccda1eb83852f
Dmitry Faddeev <faddeevd@gmail.com>
2016-04-29 15:36:44 +0300

    Initial Commit

---
+++ Calculator-SwiftTests/Calculator_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  Calculator_SwiftTests.swift
+//  Calculator-SwiftTests
+//
+//  Created by Dmitry Faddeev on 29/04/16.
+//  Copyright © 2016 Vladimir Nefedov. All rights reserved.
+//
+
+import XCTest
+@testable import Calculator_Swift
+
+class Calculator_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
