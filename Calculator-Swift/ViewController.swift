df6cee243bea9c2e02fc6b209553e09f26409686
Dmitry Faddeev <faddeevd@gmail.com>
2016-04-29 16:04:30 +0300

    start calc app

--- Calculator-Swift/ViewController.swift
+++ Calculator-Swift/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func pressButton(sender: UIButton) {
+        let digit: String? = sender.currentTitle
+        print("touched \(digit) button")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
